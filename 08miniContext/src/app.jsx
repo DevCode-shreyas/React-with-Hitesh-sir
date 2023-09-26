18482a13b7cc31e960477c5a020328214ea84f96
shreyas bole <shreyasbole4@gmail.com>
2023-09-26 17:59:48 +0530

    context api

---
+++ 08miniContext/src/app.jsx
@@ -0,0 +1,14 @@
+import "./app.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import UserContextProvider from "./context/UserContextProvider";
+
+export function App() {
+  return (
+    <UserContextProvider>
+      <h1>React with chai</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
+}
